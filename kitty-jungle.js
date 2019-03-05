794394fa489021db2ae562d6fea4bcae09ef8166
Your Name <you@example.com>
2019-03-05 16:29:03 -0500

    Gameplay Page

--- kitty-jungle.js
+++ kitty-jungle.js
@@ -1,20 +1,30 @@
-const body = document.body;
-
+//page variables
 const landingPage = document.getElementById('landing')
 const aboutPage = document.getElementById('aboutPage')
+const gamePlayPage = document.getElementById('game')
+//button variables
 const startGame = document.getElementById('startGame')
 const ruleButton= document.getElementById('goToAbout')
 const playAgain = document.getElementById('replay')
 
+//functions
 let checkRules = function(){
     landingPage.style.display='none';
     aboutPage.style.display='block';
 }
 
-ruleButton.addEventListener('click', checkRules)
-
 let goHome = function(){
     aboutPage.style.display='none';
     landingPage.style.display='block';
 }
+
+let playGame = function(){
+    landingPage.style.display='none';
+    gamePlayPage.style.display='block';
+}
+
+
+ruleButton.addEventListener('click', checkRules)
 playAgain.addEventListener('click', goHome)
+startGame.addEventListener('click', playGame)
+
