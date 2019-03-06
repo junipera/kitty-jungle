99853231d50ea02402476927a4cc4f64cb5e8d22
Your Name <you@example.com>
2019-03-06 11:51:15 -0500

    Level One Gameplay Complete

--- kitty-jungle.js
+++ kitty-jungle.js
@@ -2,7 +2,7 @@
 const landingPage = document.getElementById('landing')
 const aboutPage = document.getElementById('aboutPage')
 const gamePlayPage = document.getElementById('game')
-//level variables
+//level page variables
 const lev1 = document.getElementById('level1')
 const lev2 = document.getElementById('level2')
 const lev3 = document.getElementById('level3')
@@ -10,13 +10,16 @@ const loser = document.getElementById('lose')
 //button variables
 const startGame = document.getElementById('startGame')
 const ruleButton= document.getElementById('goToAbout')
-const playAgain = document.getElementById('replay')
+const returnHome = document.getElementById('replay')
+const playAgain = document.getElementById('replay2')
 const next = document.getElementById('arrow')
 const petButton = document.getElementById('pet')
 const petCat1 = document.getElementById('cat1')
 //additional variables
 const levelCounter = document.getElementById('levelBar')
 const friendCounter = document.getElementById('friendBar')
+let catScratch = document.getElementById('scratch')
+const happiness = document.getElementById('heart')
 let count = 0
 
 //functions
@@ -25,12 +28,21 @@ let checkRules = function(){
     aboutPage.style.display='block';
 }
 
+let refresh = function(){
+    count=0;
+    loser.style.display='none'
+    levelCounter.length = 0;
+    gamePlayPage.style.display='block';
+}
+
 let goHome = function(){
     aboutPage.style.display='none';
     landingPage.style.display='block';
+    gamePlayPage.style.display='none';
 }
 
 let playGame = function(){
+    refresh();
     landingPage.style.display='none';
     gamePlayPage.style.display='block';
     lev2.style.display='none';
@@ -41,42 +53,55 @@ let nextLevel = function(){
         count = 0
         lev1.style.display='none' 
         lev2.style.display='block'
-        levelCounter.innerHTML='Level 2'
+        levelCounter.innerHTML='<h2>Level 2</h2>'
     } 
     else if (lev2.style.display='block'){
         count = 0;
         lev2.style.display='none'
         lev3.style.display='block'
         arrow.remove()
-        levelCounter.innerHTML='Level 3'
+        levelCounter.innerHTML='<h2>Level 3</h2>'
     }
 }
 
-
 let petCat = function() {
-    const happiness = document.getElementById('heart')
     let heartFlash = setInterval(() => happiness.classList.toggle('flash'), 250);
-    setTimeout(() => { clearInterval(heartFlash)}, 1000);
-   count += 1;
-   if (count === 2) {
+    setTimeout(() => { clearInterval(heartFlash)}, 1000)
+    count += 1;
+    if (lev1.style.display='block') {
+        levelOneLoveMeter()
+    } 
+}
+
+let createBadge = function(){
     const newDiv = document.createElement('div')
     newDiv.setAttribute('class', 'emojiCat')
     friendCounter.appendChild(newDiv)
-    alert("You made a cat friend! Go to Level 2")
-    console.log(friendCounter.length)
-} else if (count >= 3) {
+}
+
+let catAttack = function (){
     happiness.style.display= 'none'
     arrow.style.display= 'none'
     lev1.style.display='none'
     loser.style.display='block'
-    alert("You went too far, and your cat friend attacked!")}
+    playAgain.style.display='inline'
+    friendCounter.removeChild(friendCounter.firstChild);
 }
 
+let levelOneLoveMeter = function(){ 
+   if (count === 2) {
+    createBadge()
+    alert("You made a cat friend! Go to Level 2")
+   } else if (count === 3) {
+    catAttack()
+    alert("You went too far, and your cat friend attacked!")
+}
+}
 
 //event listeners
 ruleButton.addEventListener('click', checkRules)
+returnHome.addEventListener('click', goHome)
 playAgain.addEventListener('click', goHome)
 startGame.addEventListener('click', playGame)
 next.addEventListener('click', nextLevel)
 petCat1.addEventListener('click', petCat)
-
